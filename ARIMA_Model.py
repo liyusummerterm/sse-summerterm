9042328e977cc7e7d28741022a7f1a3b8fac7b06
zhusq0307 <zhusq0307@163.com>
2020-07-01 12:55:05 +0800

    Cleaned data in maxmin.csv and unfinished Sesional ARIMA model

---
+++ ARIMA_Model.py
@@ -0,0 +1,124 @@
+import warnings
+import itertools
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+data = pd.read_csv('maxmin.csv', index_col=0)
+data = pd.DataFrame(data)
+data.index = pd.to_datetime(data.index)
+print(data.head())
+data = data.fillna(data.bfill())
+data.plot(figsize=(15, 6))
+#plt.show()
+
+# Define the p, d and q parameters to take any value between 0 and 2
+p = d = q = range(0, 3)
+
+# Generate all different combinations of p, q and q triplets
+pdq = list(itertools.product(p, d, q))
+
+# Generate all different combinations of seasonal p, q and q triplets
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+warnings.filterwarnings("ignore")  # specify to ignore warning messages
+
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(data['tmax'],
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
+
+            results = mod.fit()
+
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
+
+
+mod = sm.tsa.statespace.SARIMAX(data['tmax'],
+                                order=(1, 1, 1),
+                                seasonal_order=(0, 0, 1, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+
+results = mod.fit()
+
+print(results.summary().tables[1])
+
+results.plot_diagnostics(figsize=(15, 12))
+#plt.show()
+pred = results.get_prediction(start=pd.to_datetime('2020-06-09'), dynamic=False)
+pred_ci = pred.conf_int()
+ax = data['1980':].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7)
+
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Tempreture')
+plt.legend()
+
+#plt.show()
+data_forecasted = pred.predicted_mean
+data_truth = data['2020-06-09':]['tmax']
+
+# Compute the mean square error
+mse = ((data_forecasted - data_truth) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+
+pred_dynamic = results.get_prediction(start=pd.to_datetime('2020-06-09'), dynamic=True, full_results=True)
+pred_dynamic_ci = pred_dynamic.conf_int()
+ax = data['1980':]['tmax'].plot(label='observed', figsize=(20, 15))
+pred_dynamic.predicted_mean.plot(label='Dynamic Forecast', ax=ax)
+
+ax.fill_between(pred_dynamic_ci.index,
+                pred_dynamic_ci.iloc[:, 0],
+                pred_dynamic_ci.iloc[:, 1], color='k', alpha=.25)
+
+ax.fill_betweenx(ax.get_ylim(), pd.to_datetime('2020-06-09'), data['tmax'].index[-1],
+                 alpha=.1, zorder=-1)
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Tempreture')
+
+plt.legend()
+plt.show()
+
+# Extract the predicted and true values of our time-series
+data_forecasted = pred_dynamic.predicted_mean
+data_truth = data['2020-06-09':]['tmax']
+
+# Compute the mean square error
+mse = ((data_forecasted - data_truth) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+
+# Get forecast 500 steps ahead in future
+pred_uc = results.get_forecast(steps=500)
+
+# Get confidence intervals of forecasts
+pred_ci = pred_uc.conf_int()
+
+ax = data['tmax'].plot(label='observed', figsize=(20, 15))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Tempreture')
+
+plt.legend()
+plt.show()
