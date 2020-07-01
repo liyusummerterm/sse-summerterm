15bbd740cc68b02c0a0ee17cdcd05a5c29c4dc67
chestnutprog <chestnutprog@outlook.com>
2020-07-01 12:02:14 +0800

    Using Kubernetes Mirror

--- cluster/k8s/init.sh
+++ cluster/k8s/init.sh
@@ -41,14 +41,21 @@ systemctl daemon-reload
 systemctl restart docker
 systemctl enable docker
 
-
 # Install kubeadm
 
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl -s http://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+deb https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt kubernetes-xenial main
 EOF
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+kubeadm init 
