ffcaff9c6e89bba07e267dce9e51a9838378ce1a
zhusq0307 <zhusq0307@163.com>
2020-07-02 16:22:15 +0800

    Update ARIMA

--- ARIMA_Model.py
+++ ARIMA_Model.py
@@ -9,13 +9,15 @@ plt.style.use('fivethirtyeight')
 data = pd.read_csv('maxmin.csv', index_col=0)
 data = pd.DataFrame(data)
 data.index = pd.to_datetime(data.index)
-print(data.head())
+print(data['1'])
 data = data.fillna(data.bfill())
-data.plot(figsize=(15, 6))
-#plt.show()
+
+data.plot(figsize=(10, 6))
+plt.show()
+'''
 
 # Define the p, d and q parameters to take any value between 0 and 2
-p = d = q = range(0, 3)
+p = d = q = range(0, 2)
 
 # Generate all different combinations of p, q and q triplets
 pdq = list(itertools.product(p, d, q))
@@ -30,7 +32,7 @@ print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
 print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
 
 warnings.filterwarnings("ignore")  # specify to ignore warning messages
-
+"""
 for param in pdq:
     for param_seasonal in seasonal_pdq:
         try:
@@ -46,10 +48,10 @@ for param in pdq:
         except:
             continue
 
-
+"""
 mod = sm.tsa.statespace.SARIMAX(data['tmax'],
                                 order=(1, 1, 1),
-                                seasonal_order=(0, 0, 1, 12),
+                                seasonal_order=(1, 0, 1, 12),
                                 enforce_stationarity=False,
                                 enforce_invertibility=False)
 
@@ -57,11 +59,11 @@ results = mod.fit()
 
 print(results.summary().tables[1])
 
-results.plot_diagnostics(figsize=(15, 12))
-#plt.show()
-pred = results.get_prediction(start=pd.to_datetime('2020-06-09'), dynamic=False)
+results.plot_diagnostics(figsize=(10, 8))
+plt.show()
+pred = results.get_prediction(start=pd.to_datetime('2019-10-23'), dynamic=False)
 pred_ci = pred.conf_int()
-ax = data['1980':].plot(label='observed')
+ax = data['1980':]['tmax'].plot(label='observed')
 pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7)
 
 ax.fill_between(pred_ci.index,
@@ -72,15 +74,15 @@ ax.set_xlabel('Date')
 ax.set_ylabel('Tempreture')
 plt.legend()
 
-#plt.show()
+plt.show()
 data_forecasted = pred.predicted_mean
-data_truth = data['2020-06-09':]['tmax']
+data_truth = data['2015-05-01':]['tmax']
 
 # Compute the mean square error
 mse = ((data_forecasted - data_truth) ** 2).mean()
 print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
 
-pred_dynamic = results.get_prediction(start=pd.to_datetime('2020-06-09'), dynamic=True, full_results=True)
+pred_dynamic = results.get_prediction(start=pd.to_datetime('2019-10-23'), dynamic=True, full_results=True)
 pred_dynamic_ci = pred_dynamic.conf_int()
 ax = data['1980':]['tmax'].plot(label='observed', figsize=(20, 15))
 pred_dynamic.predicted_mean.plot(label='Dynamic Forecast', ax=ax)
@@ -89,7 +91,7 @@ ax.fill_between(pred_dynamic_ci.index,
                 pred_dynamic_ci.iloc[:, 0],
                 pred_dynamic_ci.iloc[:, 1], color='k', alpha=.25)
 
-ax.fill_betweenx(ax.get_ylim(), pd.to_datetime('2020-06-09'), data['tmax'].index[-1],
+ax.fill_betweenx(ax.get_ylim(), pd.to_datetime('2020-06-26'), data['tmax'].index[-1],
                  alpha=.1, zorder=-1)
 
 ax.set_xlabel('Date')
@@ -100,20 +102,20 @@ plt.show()
 
 # Extract the predicted and true values of our time-series
 data_forecasted = pred_dynamic.predicted_mean
-data_truth = data['2020-06-09':]['tmax']
+data_truth = data['2020-06-26':]['tmax']
 
 # Compute the mean square error
 mse = ((data_forecasted - data_truth) ** 2).mean()
 print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
 
 # Get forecast 500 steps ahead in future
-pred_uc = results.get_forecast(steps=500)
+pred_uc = results.get_forecast(steps=200)
 
 # Get confidence intervals of forecasts
 pred_ci = pred_uc.conf_int()
 
 ax = data['tmax'].plot(label='observed', figsize=(20, 15))
-pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+#pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
 ax.fill_between(pred_ci.index,
                 pred_ci.iloc[:, 0],
                 pred_ci.iloc[:, 1], color='k', alpha=.25)
@@ -122,3 +124,4 @@ ax.set_ylabel('Tempreture')
 
 plt.legend()
 plt.show()
+'''
