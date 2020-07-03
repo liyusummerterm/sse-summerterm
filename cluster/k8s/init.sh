e8d18ff36a2666caf861ff729b2f3377d46c9069
chestnutprog <chestnutprog@outlook.com>
2020-07-03 14:14:05 +0800

    Replace kubeadm with microk8s

--- cluster/k8s/init.sh
+++ cluster/k8s/init.sh
@@ -1,68 +1,10 @@
 #!/bin/bash
 
-## https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+snap install microk8s --classic
 
-# (Install Docker CE)
-## Set up the repository:
-### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install -y \
-  apt-transport-https ca-certificates curl software-properties-common gnupg2
+microk8s.enable dashboard dns ingress istio registry storage helm
 
-# Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+microk8s kubectl -n kube-system describe secret $token
 
-# Add the Docker apt repository:
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-
-# Install Docker CE
-apt-get update && apt-get install -y \
-  containerd.io=1.2.13-2 \
-  docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
-  docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
-
-# Set up the Docker daemon
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-mkdir -p /etc/systemd/system/docker.service.d
-# Restart Docker
-systemctl daemon-reload
-systemctl restart docker
-systemctl enable docker
-
-# Install kubeadm
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
-
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s http://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://mirrors.tuna.tsinghua.edu.cn/kubernetes/apt kubernetes-xenial main
-EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-
-kubeadm config images pull
-
-kubeadm init --pod-network-cidr=192.168.0.0/16
-
-mkdir -p /home/s18301120/.kube
-sudo cp /etc/kubernetes/admin.conf /home/s18301120/.kube/config
-sudo chown s18301120:student /home/s18301120/.kube/config
-
+microk8s.add-node
